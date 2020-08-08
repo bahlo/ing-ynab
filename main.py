81c049f467207a1620b3c940ee063aca80ed2a97
Arne Bahlo <hallo@arne.me>
2020-08-08 13:30:50 +0200

    Add YNAB create transaction logic

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-if __name__ == "__main__":
-    print("hello world")
+from datetime import datetime, date
+import json
+import os
+
+import requests
+
+BASE_URL = 'https://api.youneedabudget.com/v1'
+
+def default(o):
+    if isinstance(o, (date, datetime)):
+        return o.isoformat()
+
+if __name__ == '__main__':
+    headers = {'Authorization': "Bearer "+os.environ['YNAB_ACCESS_TOKEN']}
+    payload = {
+        'transactions': [
+            {
+                'date': datetime.now(),
+                'amount': 13.37,
+                'cleared': 'cleared',
+                'memo': 'hello from python',
+            },
+        ],
+    }
+    json_payload = json.dumps(payload, default=default)
+    print(json_payload)
+    r = requests.post(BASE_URL + '/budgets/'+os.environ['YNAB_BUDGET_ID']+'/transactions', data=json_payload, headers=headers)
+    print(r.json())
