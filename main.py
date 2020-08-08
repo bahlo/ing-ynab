099c94122075c52d8d14bb25746e09b80ded1041
Arne Bahlo <hallo@arne.me>
2020-08-08 14:13:18 +0200

    Add error handling

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ import requests
 
 BASE_URL = 'https://api.youneedabudget.com/v1'
 
+class TransactionCreationFailed(Exception):
+    def __init__(self, status_code, text):
+        self.status_code = status_code
+        self.text = text
+    def __str__(self):
+        return "Unexpeced status code %d: %s" % (self.status_code, self.text)
+
+def import_transactions(transactions):
+    headers = {'Authorization': "Bearer "+os.environ['YNAB_ACCESS_TOKEN']}
+    payload = {
+        'transactions': transactions,
+    }
+    path = '/budgets/'+os.environ['YNAB_BUDGET_ID']+'/transactions'
+    r = requests.post(BASE_URL + path, json=payload, headers=headers)
+    if r.status_code != 201:
+        raise TransactionCreationFailed(r.status_code, r.json)
+
 if __name__ == '__main__':
     load_dotenv()
     import_transactions([
@@ -18,13 +35,3 @@ if __name__ == '__main__':
             'memo': 'hello from python',
         },
     ])
-    print(r.json())
-
-
-def import_transactions(transactions):
-    headers = {'Authorization': "Bearer "+os.environ['YNAB_ACCESS_TOKEN']}
-    payload = {
-        'transactions': transactions,
-    }
-    path = '/budgets/'+os.environ['YNAB_BUDGET_ID']+'/transactions/bulk'
-    r = requests.post(BASE_URL + path, json=payload, headers=headers)
