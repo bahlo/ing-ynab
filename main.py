0be85ff8e5035a18cef179099cd8a5ef5f2472c6
Arne Bahlo <hallo@arne.me>
2020-08-08 14:17:48 +0200

    Use r.raise_for_status instead of custom exception

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import os
 from datetime import datetime, date
+import json
+import os
+
 from dotenv import load_dotenv
-import ynab
+import requests
+
+BASE_URL = 'https://api.youneedabudget.com/v1'
+
+def import_transactions(transactions):
+    headers = {'Authorization': "Bearer "+os.environ['YNAB_ACCESS_TOKEN']}
+    payload = {
+        'transactions': transactions,
+    }
+    path = '/budgets/'+os.environ['YNAB_BUDGET_ID']+'/transactions'
+    r = requests.post(BASE_URL + path, json=payload, headers=headers)
+    r.raise_for_status()
 
 if __name__ == '__main__':
     load_dotenv()
-    ynab.import_transactions([
+    import_transactions([
         {
             'account_id': os.environ['YNAB_ACCOUNT_ID'],
             'date': datetime.now().isoformat(),
