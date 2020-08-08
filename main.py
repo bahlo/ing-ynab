9de647a9bfcd9af82905593d2d7dc96902a87b0a
Arne Bahlo <hallo@arne.me>
2020-08-08 14:22:27 +0200

    Format code with black

--- main.py
+++ main.py
@@ -5,25 +5,29 @@ import os
 from dotenv import load_dotenv
 import requests
 
-BASE_URL = 'https://api.youneedabudget.com/v1'
+BASE_URL = "https://api.youneedabudget.com/v1"
+
 
 def import_transactions(transactions):
-    headers = {'Authorization': "Bearer "+os.environ['YNAB_ACCESS_TOKEN']}
+    headers = {"Authorization": "Bearer " + os.environ["YNAB_ACCESS_TOKEN"]}
     payload = {
-        'transactions': transactions,
+        "transactions": transactions,
     }
-    path = '/budgets/'+os.environ['YNAB_BUDGET_ID']+'/transactions'
+    path = "/budgets/" + os.environ["YNAB_BUDGET_ID"] + "/transactions"
     r = requests.post(BASE_URL + path, json=payload, headers=headers)
     r.raise_for_status()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     load_dotenv()
-    import_transactions([
-        {
-            'account_id': os.environ['YNAB_ACCOUNT_ID'],
-            'date': datetime.now().isoformat(),
-            'amount': 13370,
-            'cleared': 'cleared',
-            'memo': 'hello from python',
-        },
-    ])
+    import_transactions(
+        [
+            {
+                "account_id": os.environ["YNAB_ACCOUNT_ID"],
+                "date": datetime.now().isoformat(),
+                "amount": 13370,
+                "cleared": "cleared",
+                "memo": "hello from python",
+            },
+        ]
+    )
